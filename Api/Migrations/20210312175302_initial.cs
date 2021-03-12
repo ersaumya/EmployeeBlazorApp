3842d76c6ca83ee84ed8fd4c9cf7abf01377891d
ersaumya <ersaumyarout@gmail.com>
2021-03-12 23:29:19 +0530

    db migration added

---
+++ Api/Migrations/20210312175302_initial.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DoB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    Photo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "DepartmentId", "DepartmentName" },
+                values: new object[,]
+                {
+                    { 1, "IT" },
+                    { 2, "HR" },
+                    { 3, "Payroll" },
+                    { 4, "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "DepartmentId", "DoB", "Email", "FirstName", "Gender", "LastName", "Photo" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(1980, 10, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "David@gmail.com", "John", 0, "Hastings", "images/pic1.png" },
+                    { 2, 2, new DateTime(1981, 12, 22, 0, 0, 0, 0, DateTimeKind.Unspecified), "Sam@gmail.com", "Sam", 0, "Galloway", "images/pic1.jpg" },
+                    { 3, 1, new DateTime(1979, 11, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "mary@pragimtech.com", "Mary", 1, "Smith", "images/mary.png" },
+                    { 4, 3, new DateTime(1982, 9, 23, 0, 0, 0, 0, DateTimeKind.Unspecified), "sara@gmail.com", "Sara", 1, "Longway", "images/pic1.png" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
--- Web/Pages/EmpListBase.cs
+++ Web/Pages/EmpListBase.cs
@@ -25,7 +25,7 @@ namespace Web.Pages
                 Email = "ersaumyarout@gmail.com",
                 DoB = new DateTime(1990, 10, 2),
                 Gender = Gender.Male,
-                DeptId = 1,
+                DepartmentId = 1,
                 Photo = "images/pic1.jpg"
             };
             Employee e2 = new Employee
@@ -36,7 +36,7 @@ namespace Web.Pages
                 Email = "mahat@gmail.com",
                 DoB = new DateTime(1997, 10, 5),
                 Gender = Gender.Male,
-                DeptId = 2,
+                DepartmentId = 2,
                 Photo = "images/pic2.jpg"
             };
             Employees = new List<Employee> { e1, e2 };
