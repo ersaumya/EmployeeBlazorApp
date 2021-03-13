9061720646a638050f520afa597d2ed9c9a58f00
ersaumya <ersaumyarout@gmail.com>
2021-03-13 17:58:55 +0530

    Edit Form added

---
+++ Web/Pages/EditEmpBase.cs
@@ -0,0 +1,24 @@
+﻿using DomainModels;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Services;
+
+namespace Web.Pages
+{
+    public class EditEmpBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+        public Employee Employee { get; set; } = new Employee();
+
+        [Parameter]
+        public string Id { get; set; }
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
