12e7f14054e129113d880dcae58f830ece3edcbf
ersaumya <ersaumyarout@gmail.com>
2021-03-14 13:14:34 +0530

    form validation added

--- DomainModels/Employee.cs
+++ DomainModels/Employee.cs
@@ -8,11 +8,15 @@ namespace DomainModels
     public class Employee
     {
         public int EmployeeId { get; set; }
+
         [Required]
         [MinLength(3)]
         public string FirstName { get; set; }
+
         [Required]
         public string LastName { get; set; }
+
+        [EmailAddress]
         public string Email { get; set; }
         public DateTime DoB { get; set; }
         public Gender Gender { get; set; }
